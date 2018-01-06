d9589a098293dcbc6efddaf2f24f8817eb898d3f
Omer David Sivan <omerdavidsivan@gmail.com>
2018-01-06 19:34:03 +0200

    - added order range between 2 and 15. - added interface IComparable<Card> (CompareTo) - supports boolean operators

--- cardGameProj/Card.cs
+++ cardGameProj/Card.cs
@@ -8,15 +8,45 @@ namespace cardGameProj
 {
     public enum CardSuit { Spade, Diamond, Club, Heart };
 
-    class Card
+    class Card : IComparable<Card>
     {
-        public int Order { get; set; }
-        public CardSuit Suit { get; set; }
+        public int Order { get; }
+        public CardSuit Suit { get; }
 
         public Card(int order, CardSuit suit)
         {
+            if (order < 2 || order > 15)
+                throw new ArgumentException("Order must be between 2 and 15");
+
             this.Order = order;
             this.Suit = suit;
+        }
+
+        public int CompareTo(Card other)
+        {
+            if (other.Order == this.Order)
+                return other.Suit - this.Suit;
+            return other.Order - this.Order;
+        }
+
+        public static bool operator == (Card right, Card left)
+        {
+            return right.CompareTo(left) == 0;
+        }
+
+        public static bool operator != (Card right, Card left)
+        {
+            return right.CompareTo(left) != 0;
+        }
+
+        public static bool operator > (Card right, Card left)
+        {
+            return right.CompareTo(left) < 0;
+        }
+
+        public static bool operator < (Card right, Card left)
+        {
+            return right.CompareTo(left) > 0;
         }
     }
 }
--- cardGameProj/Program.cs
+++ cardGameProj/Program.cs
@@ -10,6 +10,10 @@ namespace cardGameProj
     {
         static void Main(string[] args)
         {
+            Card card1 = new Card(5, CardSuit.Diamond);
+            Card card2 = new Card(2, CardSuit.Club);
+
+            if (card1 > card2) Console.WriteLine(">");
         }
     }
 }
