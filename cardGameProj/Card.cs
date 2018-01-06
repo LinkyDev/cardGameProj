357fe2bd8d506471e185c2c20e05595361de8c18
Omer David Sivan <omerdavidsivan@gmail.com>
2018-01-06 18:55:20 +0200

    base of card created

---
+++ cardGameProj/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardGameProj
+{
+    public enum CardSuit { Spade, Diamond, Club, Heart };
+
+    class Card
+    {
+        public int Order { get; set; }
+        public CardSuit Suit { get; set; }
+
+        public Card(int order, CardSuit suit)
+        {
+            this.Order = order;
+            this.Suit = suit;
+        }
+    }
+}
